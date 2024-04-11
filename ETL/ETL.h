57ed6e322989cd22f1853ae53f5bb65a179d9182
marlonmunozscience <marlonmunozscience@gmail.com>
2024-04-11 01:00:46 -0700

    first steps

---
+++ ETL/ETL.h
@@ -0,0 +1,20 @@
+#ifndef ETL_h
+#define ETL_h
+
+#include <iostream> 
+#include <fstream>
+#include <eigen3/Eigen/Dense>
+
+class ETL
+{
+    std::string dataset;
+    std::string delimiter;
+    bool header;
+
+public:
+    ETL(std::string data, std::string separator, bool head) : dataset(data), delimiter(separator), header(head)
+    {}
+};
+
+
+#endif
